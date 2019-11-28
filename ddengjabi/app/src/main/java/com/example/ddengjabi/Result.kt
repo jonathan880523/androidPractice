4641565ab83fd224b742965c01f6dedc95efacc1
JIN SEO <seo880523@gmail.com>
2019-11-28 21:08:23 +0900

    Rest sort map.

--- ddengjabi/app/src/main/java/com/example/ddengjabi/Result.kt
+++ ddengjabi/app/src/main/java/com/example/ddengjabi/Result.kt
@@ -2,11 +2,35 @@ package com.example.ddengjabi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class Result : AppCompatActivity() {
+
+    /*********************************************************/
+    /************************전역 변수************************/
+    /*********************************************************/
+    private var resultArr: ArrayList<Int> = arrayListOf()
+    private var playerList: ArrayList<String> = arrayListOf()
+    private var saveMap = mutableMapOf<Int, String>()
+    private var sortMap = mutableMapOf<String, Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        /******************************************************/
+        /************************초기화************************/
+        /******************************************************/
+
+        //결과값 받아오기
+        resultArr = intent.getIntegerArrayListExtra("result")
+        playerList = intent.getStringArrayListExtra("player")
+
+        for (i in 0 until(resultArr.size) step 1){
+            //맵에 값 저장
+            saveMap.put(resultArr.get(i), playerList.get(i))
+        }
+
+        //TODO 솔팅할 차례
     }
 }
--- ddengjabi/app/src/main/java/com/example/ddengjabi/stopWatch.kt
+++ ddengjabi/app/src/main/java/com/example/ddengjabi/stopWatch.kt
@@ -7,21 +7,31 @@ import android.text.Editable
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_stop_watch.*
+import java.util.*
+import kotlin.concurrent.timer
 
 class stopWatch : AppCompatActivity() {
 
-    private var playerList: List<String> = arrayListOf()
+    /*********************************************************/
+    /************************전역 변수************************/
+    /*********************************************************/
+
+    private var playerList: ArrayList<String> = arrayListOf()
+    private var timeList: ArrayList<Int> = arrayListOf()
+    private var playerNo: Int = 0;
+    private var playerCnt: Int = 0;
+
+    //타이머 동작을 위한 변수
+    private var time = 0;
+    private var timerTask: Timer? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stop_watch)
 
-        //결과보기 버튼 클릭시
-        resultFab.setOnClickListener {
-            val intent = Intent(this, Result::class.java);
-            startActivity(intent);
-        }
-
+        /******************************************************/
+        /************************초기화************************/
+        /******************************************************/
         //처음엔 결과 버튼 hide
         resultFab.hide()
 
@@ -30,7 +40,12 @@ class stopWatch : AppCompatActivity() {
             //선수가 있으면 첫 선수부터 시작
             playerList = intent.getStringArrayListExtra("playerList")
 
-            playerName.text = playerList[0]
+            //게임 시작 후 첫 번째는 무조건 1번째 선수로 시작
+            showName()
+            playerCnt = playerList.count()      //등록된 선수의 수
+            //화면 이동 되면 바로 숫자 카운팅
+            start()
+
         }else{
             //등록된 선수가 없으면 선수등록 엑티비티로 이동
             Toast.makeText(this, "플레이어를 먼저 등록해주세요.", Toast.LENGTH_SHORT).show()
@@ -39,7 +54,75 @@ class stopWatch : AppCompatActivity() {
             startActivity(intent)
         }
 
+        /*****************************************************/
+        /************************Event************************/
+        /*****************************************************/
 
+        //땡잡이 버튼 클릭시
+        saveTimeButton.setOnClickListener {
+            var stopedTime = this.time
+            var lapMilli = stopedTime % 100     //멈춘 milli
+            var tens = lapMilli/10              //10의 자리 숫자
+            var ones = lapMilli - (tens * 10)   //1의 자리 숫자
+            var plus: Int = tens + ones;        //두 수의 합
+            if(plus >= 10){
+                plus = plus - 10                //두 수의 합 중 일의자리만 필요
+            }
+
+            //timeList에 선수 순서별로 시간 저장
+            timeList.add(playerNo, plus)
+
+            if(playerNo == playerCnt - 1){   //마지막 선수가 끝나면 숫자 멈춤
+                stop()
+            }else{
+                playerNo++
+                //다음 선수 보여줌
+                showName()
+            }
+        }
+
+        //결과 버튼 클릭시
+        resultFab.setOnClickListener {
+            intent = Intent(this, Result::class.java)
+            intent.putIntegerArrayListExtra("result", timeList)
+            intent.putStringArrayListExtra("player", playerList)
+            startActivity(intent)
+
+        }
+
+    }//end onCreat
+
+    /*********************************************************/
+    /************************Functions************************/
+    /*********************************************************/
+    //숫자 카운팅
+    private fun start(){
+        timerTask = timer(period = 10){
+            time++;
+
+            val sec = time / 100
+            val milli = time % 100
+
+            runOnUiThread{
+                secTextView.text = "${sec}"
+                milliTextView.text = "${milli}"
+            }
+        }
+    }
+
+    //타이머 정지 및 초기화
+    private fun stop(){
+        timerTask?.cancel()                  //시간 멈춤
+        secTextView.text = "0"                //숫자 초기화
+        milliTextView.text = "00"
+        saveTimeButton.isEnabled = false     //땡잡이 버튼 disable
+        Toast.makeText(this, "모든 플레이어가 숫자를 정했습니다. 결과 버튼을 클릭해 주세요.", Toast.LENGTH_LONG).show()
+        resultFab.show()                      //결과 버튼 visible
+    }
+
+    //플레이할 플레이어 보여주기
+    private fun showName(){
+        playerName.text = "player ${playerNo + 1}  ${playerList[playerNo]}"
     }
 
 }
