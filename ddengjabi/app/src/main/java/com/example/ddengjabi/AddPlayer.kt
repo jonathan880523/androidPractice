3fca567d7672b04b6a15e9071d9e71a5316cc988
JIN SEO <seo880523@gmail.com>
2019-12-03 21:45:52 +0900

    Fixed fined errors

--- ddengjabi/app/src/main/java/com/example/ddengjabi/AddPlayer.kt
+++ ddengjabi/app/src/main/java/com/example/ddengjabi/AddPlayer.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isEmpty
+import androidx.core.view.size
 import kotlinx.android.synthetic.main.activity_add_player.*
 import java.util.*
 import kotlin.collections.ArrayList
@@ -14,6 +17,9 @@ import kotlin.collections.mutableMapOf as mutableMapOf1
 
 class AddPlayer : AppCompatActivity() {
 
+    /*********************************************************/
+    /************************전역 변수************************/
+    /*********************************************************/
     private var order: Int = 1
     private var idx: Int = 1
     private var playerList: ArrayList<String> = arrayListOf<String>()
@@ -22,14 +28,26 @@ class AddPlayer : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_player)
 
+        /*****************************************************/
+        /************************Event************************/
+        /*****************************************************/
         //게임 시작 버튼 클릭시
         startGameButton.setOnClickListener {
-            val intent = Intent(this, stopWatch::class.java)
-            intent.putStringArrayListExtra("playerList", playerList)
-            startActivity(intent)
+            var inputedName = TextView(this)
+            if(lapLayOut.isEmpty()){
+                Toast.makeText(this, "플레이어를 입력해주세요.", Toast.LENGTH_LONG).show()
+
+            }else if(idx < 3){
+               Toast.makeText(this, "최소 2명의 플레이어가 필요합니다.", Toast.LENGTH_LONG).show()
+
+            }else{
+                val intent = Intent(this, stopWatch::class.java)
+                intent.putStringArrayListExtra("playerList", playerList)
+                startActivity(intent)
+
+            }
         }
-        
-        
+
         //플레이어 추가 버튼 클릭시
         addInputLineFab.setOnClickListener {
             val sPlayerName = inputNameEditBox.text
@@ -54,6 +72,7 @@ class AddPlayer : AppCompatActivity() {
                 Toast.makeText(this, "이름을 입력해주세요.", Toast.LENGTH_LONG).show()
             }
         }
+
     }
 }
 
